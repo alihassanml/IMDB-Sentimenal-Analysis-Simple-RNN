e03a3761001a196f93550c49693d04f9d5b28b2b
Ali Hassan <165924119+alihassanml@users.noreply.github.com>
2024-08-03 23:17:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
